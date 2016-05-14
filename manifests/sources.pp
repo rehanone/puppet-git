238c395822e20bf41ee873ba5c5bba74d8da16ed
Rehan Mahmood <rehanone@gmail.com>
2016-05-14 08:22:14 -0400

    Fix strict variable checking issue with use of facts hash in newer version of puppet.

--- manifests/sources.pp
+++ manifests/sources.pp
@@ -3,7 +3,7 @@ class git::sources {
 
   # assert_private("Use of private class ${name} by ${caller_module_name}")
 
-  case $::osfamily {
+  case $facts['osfamily'] {
     'Ubuntu': {
       contain apt
       apt::ppa { $git::sources_repo:
