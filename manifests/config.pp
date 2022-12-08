a4e07fb1b56380adf72d3ac414bbbd71d1a93aca
Rehan Mahmood <rehanone@gmail.com>
2022-12-07 20:32:16 -0500

    - Updated os support matrix. - Updated `pdk` templates.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,7 +1,6 @@
 # Class: git::config
 #
 class git::config () inherits git {
-
   assert_private("Use of private class ${name} by ${caller_module_name}")
 
   create_resources('git::user', $git::users)
